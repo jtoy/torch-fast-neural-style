d91967a84462469511235180dc446f8941aeb8b4
lqfortytwo <lqfmickey@gmail.com>
2016-10-05 14:42:15 -0400

    Minor fix

--- inference.sh
+++ inference.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 OUTPUT="/data/output/$(date +%s).jpg"
 #script
-th fast_neural_style.lua $@ -output_dir "$OUTPUT"
+/home/ubuntu/torch/install/bin/th fast_neural_style.lua $@ -output_dir "$OUTPUT"
