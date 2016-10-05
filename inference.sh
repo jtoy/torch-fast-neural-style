46c6ede3682b34b123b08e3d8896f9ac9ea38405
lqfortytwo <lqfmickey@gmail.com>
2016-10-05 14:58:24 -0400

    echo output

--- inference.sh
+++ inference.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 OUTPUT="/data/output/$(date +%s).jpg"
 #script
-/home/ubuntu/torch/install/bin/th fast_neural_style.lua $@ -output_dir "$OUTPUT"
+/home/ubuntu/torch/install/bin/th fast_neural_style.lua $@ -output_image "$OUTPUT"
+
+echo $OUTPUT
