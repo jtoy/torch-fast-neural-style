faa822739928bb539736f1347cd55ec110996053
lqfortytwo <lqfmickey@gmail.com>
2016-10-09 17:36:27 -0400

    fix typo

--- train.lua
+++ train.lua
@@ -21,7 +21,7 @@ Train a feedforward style transfer model
 cmd:option('-arch', 'c9s1-32,d64,d128,R128,R128,R128,R128,R128,u64,u32,c9s1-3')
 cmd:option('-use_instance_norm', 1)
 cmd:option('-task', 'style', 'style|upsample')
-cmd:option('-h5_file', '/data/data_setsc/ms-coco-256.h5')
+cmd:option('-h5_file', '/data/data_sets/ms-coco-256.h5')
 cmd:option('-padding_type', 'reflect-start')
 cmd:option('-tanh_constant', 150)
 cmd:option('-preprocessing', 'vgg')
